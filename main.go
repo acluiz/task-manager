29eb59f8ab11b3e1db4ca2a843b2906d3c2a20d0
Ana Luiz <anamurdock881@gmail.com>
2024-05-20 20:33:26 -0300

    feat: add main and config files

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"task-manager/configs"
+	"task-manager/handlers"
+
+	"github.com/go-chi/chi"
+)
+
+func main() {
+	err := configs.Load()
+	
+	if err != nil {
+		panic(err)
+	}
+
+	r := chi.NewRouter()
+
+	r.Post("/", handlers.Create)
+	r.Get("/", handlers.GetAll)
+	r.Get("/${id}", handlers.Get)
+	r.Put("/{id}", handlers.Update)
+	r.Delete("/{id}", handlers.Delete)
+	
+	http.ListenAndServe(fmt.Sprintf(":%s", configs.GetServerPort()), r)
+}
